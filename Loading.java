a5d126c2a0f5bb94b8065d4cba472faacc88e649
KP-R76 <126742238+KP-R76@users.noreply.github.com>
2023-03-02 10:30:29 +0700

    Update Loading.java

--- Loading.java
+++ Loading.java
@@ -1,1 +1,1 @@
-[{"load1":"KP-R76/Loading.java","load2":"Download Database","load3":"Loading.. 75/100","load4":"Loading 100/100"}]
+[{"load1":" KP-R76/Loading.java ","load2":"Download Database","load3":"Loading.. 75/100","load4":"Loading 100/100"}]
